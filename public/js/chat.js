d5dc4922e43f5e2edf0a42bbd1c9b71ded7b9627
aryanag <hansy.agarwal@gmail.com>
2020-04-24 02:20:13 +0530

    track and scroll

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,17 +10,62 @@ const $messages = document.querySelector('#messages')
 //templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 //options
 const {username, room} = Qs.parse(location.search, {ignoreQueryPrefix: true})
 
+const autoscroll = ()=>{
+    //new message elemet
+    const $newMessage = $messages.lastElementChild
+
+    //height of new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    //visible height
+    const visibleHeight = $messages.offsetHeight
+
+    //Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    //How far have i scrolled
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight-newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
 socket.on('message',(message)=>{
     console.log(message)
     const html = Mustache.render(messageTemplate, {
+        username: message.username,
         message: message.text,
         createdAt: moment(message.createdAt).format('hh:mm a')
     })
-    $messages.insertAdjacentHTML('beforebegin', html)
+    $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
+})
+
+socket.on('locationMessage', (url)=>{
+    const html = Mustache.render(locationTemplate, {
+        username: url.username,
+        url: url.url,
+        createdAt: moment(url.createdAt).format('hh:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+    //console.log(url)
+    autoscroll()
+})
+
+socket.on('roomData', ({room,users})=>{
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit',(e)=>{
@@ -44,15 +89,6 @@ $messageForm.addEventListener('submit',(e)=>{
     })
 })
 
-socket.on('locationMessage', (url)=>{
-    const html = Mustache.render(locationTemplate, {
-        url: url.url,
-        createdAt: moment(url.createdAt).format('hh:mm a')
-    })
-    $messages.insertAdjacentHTML('beforebegin', html)
-    //console.log(url)
-})
-
 $geolocationButton.addEventListener('click',()=>{
     if(!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
@@ -64,7 +100,6 @@ $geolocationButton.addEventListener('click',()=>{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, (error)=>{
-
             $geolocationButton.removeAttribute('disabled')
             if(error) {
                 return console.log('error')
@@ -74,4 +109,9 @@ $geolocationButton.addEventListener('click',()=>{
     })
 })
 
-socket.emit('join', {username, room})
+socket.emit('join', {username, room}, (error)=>{
+    if(error) {
+        alert(error)
+        location.href = '/'
+    }
+})
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const socketio = require('socket.io')
 const Filter =  require('bad-words')
 const {generateMessage, generateLocationMessage } = require('./utils/messages')
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -18,34 +19,56 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection',(socket)=>{
     console.log('New webSocket connection')
     
-    socket.on('join', ({username, room})=>{
-        socket.join(room)
-        //to send to socket only
-        socket.emit('message', generateMessage('Welcome!'))
-        //to send to everyone except the socket
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined`))
+    socket.on('join', ({username, room}, callback)=>{
+        const {error, user} = addUser({id: socket.id, username, room})
+        
+        if(error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
+        
+        socket.emit('message', generateMessage('Admin','Welcome!')) //to send to socket only
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin',`${user.username} has joined`))//to send to everyone except the socket
         //io.to.emit send message to everyone in room       
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+        callback()
     })
 
     socket.on('sendMessage',(message,callback) => {
         const filter = new Filter()
+        const user = getUser(socket.id)
 
         if(filter.isProfane(message)) {
             return callback('Profanity is not allowed')
         }
         //to send to everyone
-        io.to('Jaipur').emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username,message))
         callback()
     })
 
     socket.on('sendLocation',(coords, callback)=>{
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username,`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
     //when a user disconnects
     socket.on('disconnect', ()=>{
-        io.emit('message', generateMessage('A user has left'))
+        const user = removeUser(socket.id)
+
+        if(user){
+            io.to(user.room).emit('message', generateMessage('Admin',`${user.username} has left`))
+            io.to(user.room).emit('roomData',{
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+
+        
     })
 })
 
